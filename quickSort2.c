0387d723ee811492214434a116c2437c7391a898
Caleb Oglesby <noobyman68@gmail.com>
2025-08-12 22:55:24 -0400

    updated quickSort2.c to be able to handle multiple values that are equal, the program is not working in this state. Debugging needs to be done on partitionInt function

--- quickSort2.c
+++ quickSort2.c
@@ -3,40 +3,86 @@
 #include "string.h"
 #include "stdbool.h"
 
-int partitionInt(int *list, int size){
+
+static void swap(int *list1, int index1, int *list2, int index2){
+  list1[index1] = list1[index1] ^ list2[index2];
+  list2[index2] = list1[index1] ^ list2[index2];
+  list1[index1] = list1[index1] ^ list2[index2];
+}
+
+
+int* partitionInt(int *list, int size){
   if(size <= 1){
-    return 0;
+    return list;
   }
-  int left = 0;
-  int right = size-1;
+  int left = 0; 
+  int mid = 0;  
   int median = list[size/2];
+
   int Lcount = 0;
+  int Mcount = 0;
+  int Rcount = 0;
+
   for(int i = 0; i < size; i++){
     if(list[i] < median){
       Lcount++;
+    }else if(list[i] == median){
+      Mcount++;
+    }else{
+      Rcount++;
     }
   }
+
+  int right = Rcount-1;
+
+  int *Left = malloc(Lcount * sizeof(int));
+  int *Mid = malloc(Mcount * sizeof(int));
+  int *Right = malloc(Rcount * sizeof(int));
+
+  memcpy(Left, list, Lcount);
+  memcpy(Mid, list+Lcount, Mcount);
+  memcpy(Right, list+Rcount, Rcount);
+
   while(true){
-    printf("%d ", list[left]);
-    printf("%d ", median);
-    while(list[left] < median){
+    while(Left[left] < median && left < Lcount){
       left++;
-      printf("test1");
     }
-    while(list[right] > median){
+    while(Right[right] > median && right >= 0){
       right--;
-      printf("test2");
     }
-    if(left>=right){
+    //even if an out of range value is read, it should never get used
+    //if all values in left have been checked then there will be no value for right to switch with either
+    if(left >= Lcount){
       break;
     }
-    list[left] = list[left] ^ list[right];
-    list[right] = list[left] ^ list[right];
-    list[left] = list[left] ^ list[right];
+    
+    if(Left[left] == median){
+      swap(Left, left, Mid, mid);
+      mid++;
+      continue;
+    }else if(Right[right] == median){
+      swap(Right, right, Mid, mid);
+      mid++;
+      continue;
+    }
+    swap(Left, left, Right, right);
   }
-  partitionInt(list, Lcount);
-  partitionInt(list+Lcount,size-Lcount);
-  return 0;
+  left -= Lcount;
+  right -= Rcount;
+  mid -= Mcount;
+
+  int *sorted = malloc(size*sizeof(int));
+
+  memcpy(sorted, partitionInt(Left, Rcount), Lcount);
+  free(Left);
+  
+  memcpy(sorted+Lcount, Mid, Mcount);
+  free(Mid);
+
+  memcpy(sorted, partitionInt(Right, Rcount), Rcount);
+  free(Right);
+
+  return sorted;
 }
 int* quickInt(int *list, int size){
   int *sortedList = (int*)malloc(size * sizeof(int));
